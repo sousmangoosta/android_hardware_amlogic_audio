34d62d4d4e1de910e4d56b0dc2f8f0ff8fdb5910
Zhe Wang <zhe.wang@amlogic.com>
2017-02-24 19:16:14 +0800

    PD#137760: fixed error merge of 36e70cf30
    Change-Id: Ia1097e40f840269ccb08c049b6dd30255c491e07
    Signed-off-by: Zhe Wang <zhe.wang@amlogic.com>

--- libTVaudio/audio/android_out.h
+++ libTVaudio/audio/android_out.h
@@ -8,7 +8,7 @@ extern "C" {
 int new_android_audiotrack(void);
 int release_android_audiotrack(void);
 int amsysfs_get_sysfs_int(const char *path);
-
+int release_raw_audio_track(void);
 
 #ifdef __cplusplus
 }
