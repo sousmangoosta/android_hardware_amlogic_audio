1741cef8c63948df0450317f108f3948f56ad5b2
Shuai Li <shuai.li@amlogic.com>
2017-03-06 17:08:54 +0800

    PD#139050: audio: fix wrong DTS_HD enum string
    Change-Id: If446af6ae8f356d91bdd4d1b2d7217df5a3200da

--- audio_hw_profile.c
+++ audio_hw_profile.c
@@ -165,7 +165,7 @@ char*  get_hdmi_sink_cap(const char *keys)
                 size += sprintf(aud_cap + size, "|%s", "AUDIO_FORMAT_AC3");
             }
             if (mystrstr(infobuf, "DTS-HD")) {
-                size += sprintf(aud_cap + size, "|%s", "AUDIO_FORMAT_DTS|AUDIO_FORMAT_DTSHD");
+                size += sprintf(aud_cap + size, "|%s", "AUDIO_FORMAT_DTS|AUDIO_FORMAT_DTS_HD");
             } else if (mystrstr(infobuf, "DTS")) {
                 size += sprintf(aud_cap + size, "|%s", "AUDIO_FORMAT_DTS");
             }
